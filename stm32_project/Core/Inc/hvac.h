8ae67f1cb13dfff17493525a7d20679df36f011f
SangJun <winwinecosystem@gmail.com>
2025-01-22 11:50:14 +0900

    add hvac

---
+++ stm32_project/Core/Inc/hvac.h
@@ -0,0 +1,26 @@
+/*
+ * hvac.h
+ *
+ *  Created on: Jan 22, 2025
+ *      Author: USER
+ */
+
+#ifndef INC_HVAC_H_
+#define INC_HVAC_H_
+
+extern uint8_t heater_led_state;
+extern uint8_t ac_led_state;
+extern uint8_t pan_state;
+extern uint8_t heater_power;
+extern uint8_t ac_power;
+
+void driver_ac(uint8_t state);
+void driver_heater(uint8_t state);
+void smart_ac(uint8_t state);
+void smart_heater(uint8_t state);
+void on_heater(void);
+void off_heater(void);
+void on_ac(void);
+void off_ac(void);
+
+#endif /* INC_HVAC_H_ */
